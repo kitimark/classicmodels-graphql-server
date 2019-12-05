ea848d649487545e0fbe09856edaa5a9259e7273
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-05 11:08:32 +0700

    Initialize project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+import resolvers from './resolvers'
+
+const server = new GraphQLServer({
+  typeDefs: `./src/schema.graphql`,
+  resolvers
+})
+
+const options = {
+  port: 4000
+}
+
+server.start(options, () => {
+  console.log(`Server is running on port ${options.port}`)
+})
