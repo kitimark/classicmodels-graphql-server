50f8af34591b3ad7d5a72cd69e4031251627c8d5
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-08 12:14:03 +0700

    Update customer, employee query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,7 +1,11 @@
 export const Query = {
   ping: (): string => 'hello world',
+  customer: (_parent: any, { id }: any, { models }: any ) =>
+    models.Customer.findById(id).exec(),
   customers: (_parent: any, _args: any, { models }: any) => 
     models.Customer.find({}).exec(),
+  employee: (_parent: any, { id }: any, { models }: any) =>
+    models.Employee.findById(id).exec(),
   employees: (_parent: any, _args: any, { models }: any) =>
     models.Employee.find({}).exec(),
   products: (_parent: any, _args: any, { models }: any) =>
