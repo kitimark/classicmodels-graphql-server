1bb55cef500edf8b02544f3de1a74d9029c89daa
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-11 12:44:17 +0700

    Add remove customer

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -2,5 +2,6 @@ export const Mutation = {
   createCustomer: (_parent: any, { input }: any, { models }: any) => {
     const tranlate_input = models.Customer.translateAliases(input)
     return models.Customer.create(tranlate_input)
-  }
+  },
+  removeCustomer: (_parent: any, { id }: any, { models }: any) => models.Customer.findByIdAndRemove(id)
 }
