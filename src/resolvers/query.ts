5c1743ae06102449f08f5bcc052915d6cc2d3cac
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-12 10:36:49 +0700

    Update authorize in query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,19 +1,31 @@
+const authorize = (req: any) => {
+  if (!req.session.employee) {
+    throw new Error('Unauthorized')
+  }
+}
+
 export const Query = {
   ping: (): string => 'hello world',
   me: (_parent: any, _args: any, { req }: any) => {
-    if (!req.session.employee) {
-      throw new Error('Unauthorized')
-    }
+    authorize(req)
     return req.session.employee
   },
-  customer: (_parent: any, { id }: any, { models }: any) =>
-    models.Customer.findById(id).exec(),
-  customers: (_parent: any, _args: any, { models }: any) =>
-    models.Customer.find({}).exec(),
-  employee: (_parent: any, { id }: any, { models }: any) =>
-    models.Employee.findById(id).exec(),
-  employees: (_parent: any, _args: any, { models }: any) =>
-    models.Employee.find({}).exec(),
+  customer: (_parent: any, { id }: any, { models, req }: any) =>{
+    authorize(req)
+    return models.Customer.findById(id).exec()
+  },
+  customers: (_parent: any, _args: any, { models, req }: any) => {
+    authorize(req)
+    return models.Customer.find({}).exec()
+  },
+  employee: (_parent: any, { id }: any, { models, req }: any) => {
+    authorize(req)
+    return models.Employee.findById(id).exec()
+  },
+  employees: (_parent: any, _args: any, { models, req }: any) => {
+    authorize(req)
+    return models.Employee.find({}).exec()
+  },
   product: (_parent: any, args: any, { models }: any) => {
     const tranlate_args = models.Product.translateAliases(args)
     return models.Product.findOne(tranlate_args).exec()
@@ -28,10 +40,14 @@ export const Query = {
     models.Office.find({}).exec(),
   payments: (_parent: any, args: any, { models }: any) =>
     models.Payment.find({}).exec(),
-  orders: (_parent: any, args: any, { models }: any) =>
-    models.Order.find({}).exec(),
-  coupons: (_parent: any, args: any, { models }: any) =>
-    models.Coupon.find({}).exec(),
+  orders: (_parent: any, args: any, { models, req }: any) => {
+    authorize(req)
+    return models.Order.find({}).exec()
+  },
+  coupons: (_parent: any, args: any, { models, req }: any) => {
+    authorize(req)
+    models.Coupon.find({}).exec()
+  },
   scaleList: async (_parent: any, _args: any, { models }: any) => {
     const result: Array<any> = await models.Product.aggregate([
       {
