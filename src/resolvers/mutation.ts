10bcb461d916a758d75c28b9f89819241ca203f0
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-12 13:48:42 +0700

    Add update customer

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,11 @@
 import { authorize } from '../services/authenticate'
+import { Types } from 'mongoose'
 
 export const Mutation = {
   login: async (_parent: any, { credentials }: any, { req, services }: any) => {
-    const employee = await services.authentication.verifyCredentials(credentials)
+    const employee = await services.authentication.verifyCredentials(
+      credentials
+    )
     req.session.employee = employee
     return employee
   },
@@ -13,8 +16,12 @@ export const Mutation = {
     delete req.session.employee
     return { message: 'Logged out successfully' }
   },
-  createEmployee: async (_parent: any, { input }: any, { models, services ,req}: any) => {
-    authorize(req ,['VP Sale','President'])
+  createEmployee: async (
+    _parent: any,
+    { input }: any,
+    { models, services, req }: any
+  ) => {
+    authorize(req, ['VP Sale', 'President'])
     // hash password
     const hashedPassword = await services.hasher.hashPassword(input.password)
     input.password = hashedPassword
@@ -23,8 +30,8 @@ export const Mutation = {
     // TODO: add reportsTo property
     return models.Employee.create(tranlate_input)
   },
-  removeEmployee: async (_parent: any, { id }: any, { models , req }: any) => {
-    authorize(req ,['VP Sale','President'])
+  removeEmployee: async (_parent: any, { id }: any, { models, req }: any) => {
+    authorize(req, ['VP Sale', 'President'])
     const deleteEmployee = await models.Employee.findByIdAndRemove(id)
     return deleteEmployee
   },
@@ -32,24 +39,35 @@ export const Mutation = {
     const tranlate_input = models.Customer.translateAliases(input)
     return models.Customer.create(tranlate_input)
   },
-  removeCustomer: async (_parent: any, { id }: any, { models,req }: any) => {
-    authorize(req )
+  updateCustomer: async (
+    _parent: any,
+    { id, input }: any,
+    { models, req }: any
+  ) => {
+    authorize(req)
+    const updateUser = await models.Customer.findOneAndUpdate({
+      _id: Types.ObjectId(id)
+    })
+    return updateUser
+  },
+  removeCustomer: async (_parent: any, { id }: any, { models, req }: any) => {
+    authorize(req)
     // TODO: Check permission
     const deletedUser = await models.Customer.findByIdAndRemove(id)
     return deletedUser
   },
-  removeProduct: async (_parent: any, { id }: any, { models }: any) => { 
+  removeProduct: async (_parent: any, { id }: any, { models }: any) => {
     const deleteProduct = await models.Product.findByIdAndRemove(id)
     return deleteProduct
   },
-  createCoupon: async (_parent: any, { input }: any, { models ,req}: any) => {
-    authorize(req ,['Vp Marketing','President'])
+  createCoupon: async (_parent: any, { input }: any, { models, req }: any) => {
+    authorize(req, ['Vp Marketing', 'President'])
     input.remainder = input.totallity
     const tranlate_input = models.Coupon.translateAliases(input)
-    return models.Coupon.create(tranlate_input) 
+    return models.Coupon.create(tranlate_input)
   },
-  removeCoupon: async (_parent: any, { id }: any, { models , req }: any) => {
-    authorize(req ,['Vp Marketing','President'])
+  removeCoupon: async (_parent: any, { id }: any, { models, req }: any) => {
+    authorize(req, ['Vp Marketing', 'President'])
     const deleteCoupon = await models.Coupon.findByIdAndRemove(id)
     return deleteCoupon
   }
